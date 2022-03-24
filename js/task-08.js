e766d89b588cbc76ec2e025277d1aa27374f93d7
HryhoriiAndriiets1974 <ander_1_agg@ukr.net>
2022-03-24 10:20:55 +0200

    hw-06-task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,12 @@
-
+const inputForm = document.querySelector(".login-form");
+const inputField = inputForm.querySelectorAll("input");
+inputForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const email = e.currentTarget.elements.email.value;
+  const password = e.currentTarget.elements.password.value;
+  if (email === "" || password === "") {
+    alert("Заповніть всі поля для введення !!!");
+  } else {let fillingForm = {email, password,};
+          console.log(fillingForm);}
+  inputForm.reset();
+});
