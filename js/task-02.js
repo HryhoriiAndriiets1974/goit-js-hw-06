b814ddc5875f4b235b6055ba3448c70499db3d1d
HryhoriiAndriiets1974 <ander_1_agg@ukr.net>
2022-03-26 10:08:19 +0200

    hw-06-task-02-modern

--- js/task-02.js
+++ js/task-02.js
@@ -7,12 +7,17 @@ const ingredients = [
   'Condiments',
 ];
 const listIngridients = document.querySelector("#ingredients");
-const listItems = [];
-for (const element of ingredients) {
+const listItems = ingredients.map ((element) => {
   const li = document.createElement("li");
   li.classList.add("item");
   li.textContent = element;
-  listItems.push(li);
-}
+  return li;
+});
+// for (const element of ingredients) {
+//   const li = document.createElement("li");
+//   li.classList.add("item");
+//   li.textContent = element;
+//   listItems.push(li);
+// }
 listIngridients.append(...listItems);
 console.log(listIngridients);
