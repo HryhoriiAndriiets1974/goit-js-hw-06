fbfdcd662341166d3732f9872a0742860afde553
HryhoriiAndriiets1974 <ander_1_agg@ukr.net>
2022-03-26 13:34:08 +0200

    hw-06-task-07-???

--- js/task-07.js
+++ js/task-07.js
@@ -1,5 +1,6 @@
 const inputControl = document.getElementById("font-size-control");
 const fontSize = document.getElementById("text");
+fontSize.style.fontSize = inputControl.value;
 inputControl.addEventListener("input", (event) => {
   const currentSize = event.target.value;
   fontSize.style.fontSize = `${currentSize}px`;
