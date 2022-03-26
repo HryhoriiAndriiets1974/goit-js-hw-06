ecea906be7dc6979c5f723a94da6f4b49d45abae
HryhoriiAndriiets1974 <ander_1_agg@ukr.net>
2022-03-26 10:52:14 +0200

    hw-06-task-04-???

--- js/task-04.js
+++ js/task-04.js
@@ -1,11 +1,31 @@
-let counterValue = document.querySelector("#value");
-const button = document.querySelectorAll("button");
+// --------------------- 1 old
+// let counterValue = document.querySelector("#value");
+// const button = document.querySelectorAll("button");
 
-button[0].addEventListener("click", () => {
-  counterValue.textContent -= 1;
+// button[0].addEventListener("click", () => {
+//   counterValue.textContent -= 1;
 
-});
-button[1].addEventListener("click", () => {
-  counterValue.textContent =
-  parseInt(document.querySelector("#value").textContent) + 1;
-});
+// });
+// button[1].addEventListener("click", () => {
+//   counterValue.textContent =
+//   parseInt(document.querySelector("#value").textContent) + 1;
+// });
+// ---------------------- 2 new
+const counterValue = {
+  value: 0,
+  add() {this.value += 1;},
+  dec() {this.value -= 1;},
+};
+const btnElAdd = document.querySelector('[data-action="increment"]');
+const btnElAddClick = () => {
+  counterValue.add();
+  value.textContent = counterValue.value;
+};
+btnElAdd.addEventListener('click', btnElAddClick);
+
+const btnElDec = document.querySelector('[data-action="decrement"]');
+const btnElDecClick = () => {
+  counterValue.dec();
+  value.textContent = counterValue.value;
+};
+btnElDec.addEventListener('click', btnElDecClick);
